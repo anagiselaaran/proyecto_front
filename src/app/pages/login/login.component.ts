a4ec1788963f4d9c5247a566b361af4fcd3c5a12
Camsimew <camilaerasso@gmail.com>
2024-07-18 13:09:13 +0200

    Maquetacion form login, servicios, interfaces

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+
+}
+
