331279e6aaf4ac69861e4181a7f1733848930137
Ana Aran <anita.gisela.aran@gmail.com>
2024-07-19 11:01:26 +0200

    creacion html admin page y creacion (vacia!) de card empleados

---
+++ src/app/components/card-trabajadores/card-trabajadores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-trabajadores',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-trabajadores.component.html',
+  styleUrl: './card-trabajadores.component.css'
+})
+export class CardTrabajadoresComponent {
+
+}
