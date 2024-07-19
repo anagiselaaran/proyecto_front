b295c92f3ab742bd7385a5e1a4c0b0220aae6962
Jona Jimenez <jona.jim.g@gmail.com>
2024-07-19 11:45:13 +0200

    Comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProyectosComponent } from "./pages/proyectos/proyectos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProyectosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/services/proyectos.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { firstValueFrom } from 'rxjs';
+import { Proyecto } from '../interfaces/proyecto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProyectosService {
+
+  private baseUrl = `${environment.apiUrl}/api/crearproyecto`;
+
+  private httpClient = inject(HttpClient);
+
+  createProyecto(nuevoProyecto: Proyecto): Promise<Proyecto> {
+    return firstValueFrom(
+      this.httpClient.post<Proyecto>(this.baseUrl, nuevoProyecto)
+    );
+  }
+
+  getById(proyectoId: number): Promise<Proyecto> {
+    return firstValueFrom(
+      this.httpClient.get<Proyecto>(`${this.baseUrl}/${proyectoId}`)
+    );
+  }
+
+    getAll(): Promise<Proyecto[]> {
+    return firstValueFrom(
+      this.httpClient.get<Proyecto[]>(this.baseUrl)
+    )
+  }
+}
