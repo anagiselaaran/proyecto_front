daccb5e671cc39f168c768d427dcc90d54a31887
Jona Jimenez <jona.jim.g@gmail.com>
2024-07-22 10:49:27 +0200

    Ruta proyectos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: '/login' },
+    { path: 'proyectos', component: ProyectosComponent }
 ];
