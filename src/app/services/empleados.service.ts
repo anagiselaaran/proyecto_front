d9da10c7f405ecb1b2d9f85483b035cc363db5d3
André Candeloro <candeloro95@gmail.com>
2024-07-23 12:20:43 +0200

    Fixed merge leftover conflict

--- src/app/services/empleados.service.ts
+++ src/app/services/empleados.service.ts
@@ -1,25 +1,18 @@
 import { Injectable, inject } from '@angular/core';
-<<<<<<< HEAD
-
-=======
->>>>>>> 705ba188fe880efc7a2166c9e700a660fe71f163
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Empleado } from '../interfaces/empleado.interface';
 import { firstValueFrom } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 
-type LoginBody = { email: string, password: string };
-type ApiResponse = { success: string, token?: string };
-
+type LoginBody = { email: string; password: string };
+type ApiResponse = { success: string; token?: string };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmpleadosService {
-
   private baseUrl = `${environment.apiUrl}/api/users`;
   private httpClient = inject(HttpClient);
-
 
   login(body: LoginBody): Promise<ApiResponse> {
     return firstValueFrom(
@@ -27,12 +20,7 @@ export class EmpleadosService {
     );
   }
 
-
-
   getAll(): Promise<Empleado[]> {
-    return firstValueFrom(
-      this.httpClient.get<Empleado[]>(this.baseUrl)
-
-    );
+    return firstValueFrom(this.httpClient.get<Empleado[]>(this.baseUrl));
   }
 }
