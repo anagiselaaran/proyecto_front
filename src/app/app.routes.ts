c272d2f65c6c06fc0696cf115bd9970d6a46f185
André Candeloro <candeloro95@gmail.com>
2024-07-23 17:44:58 +0200

    Created time Services with methods

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { roleGuardGuard } from './guards/role-guard.guard';
 import { authGuard } from './guards/auth.guard';
 import { RegisterComponent } from './pages/register/register.component';
+import { TimerPageComponent } from './pages/timer-page/timer-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'admin', component: AdminPageComponent, canActivate: [authGuard, roleGuardGuard] },
+    { path: "timer", component: TimerPageComponent },
     /*  { path: 'projects/:projectId', component: ,canActivate: [authGuard, roleGuardGuard]} para Cami*/
 
     { path: '**', redirectTo: '/login' },
---
+++ src/app/interfaces/time.interface.ts
@@ -0,0 +1,6 @@
+export interface Time {
+  id: number;
+  date: string;
+  work_hours_ms: number;
+  id_user: number;
+}
