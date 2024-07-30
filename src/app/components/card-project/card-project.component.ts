9596580a2a6f9342b6812356264dcc88d2f0dc40
luis cardenes <luiscardenes21@gmail.com>
2024-07-30 17:21:17 +0100

    haciendo pull

--- src/app/components/card-project/card-project.component.ts
+++ src/app/components/card-project/card-project.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Proyecto } from '../../interfaces/proyecto.interface';
 import { RouterLink } from '@angular/router';
 import { DatePipe } from '@angular/common';
+import { DptoTransformPipe } from '../../pipes/dpto-transform.pipe';
 
 @Component({
   selector: 'card-project',
   standalone: true,
-  imports: [RouterLink,DatePipe],
+  imports: [RouterLink,DatePipe, DptoTransformPipe],
   templateUrl: './card-project.component.html',
   styleUrl: './card-project.component.css'
 })
