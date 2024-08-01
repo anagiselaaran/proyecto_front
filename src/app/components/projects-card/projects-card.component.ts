361d354a8198a32bff23a55bb08d3e1930aed4c7
Camsimew <158579081+Camsimew@users.noreply.github.com>
2024-08-01 16:45:45 +0200

    Mejora diseño dront registro proyecto

---
+++ src/app/components/projects-card/projects-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'projects-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects-card.component.html',
+  styleUrl: './projects-card.component.css'
+})
+export class ProjectsCardComponent {
+
+}
